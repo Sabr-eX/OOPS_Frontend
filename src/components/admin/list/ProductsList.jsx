3dd0acbf7148f9c6e5cee98a04868e204205fdda
Kush <kushpagrawal@gmail.com>
2022-12-08 00:13:31 +0530

    add admin - user & product

--- src/components/admin/list/ProductsList.jsx
+++ src/components/admin/list/ProductsList.jsx
@@ -1,26 +1,45 @@
 import styled from "styled-components";
-import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import {useDispatch, useSelector} from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { productsDelete } from "../../../slices/productsSlice";
-import EditProduct from "../EditProduct";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+// import EditProduct from "../EditProduct";
 
 
 export default function ProductsList() {
-  const dispatch = useDispatch();
-const navigate = useNavigate();
-  // const {items}=useSelector((state)=>state.products)
 
-//   const rows=items&&items.map(item=>{
-//     return{
-//       id: item._id,
-//       imageUrl:item.image.url,
-//       pName:item.name,
-//       pDesc:item.desc,
-//       price: item.price.toLocaleString()
-//     };
-//   });
+const [product, setProduct] = useState();
+
+useEffect(() => {
+  const getProducts = async () => {
+    await axios.get('https://gada-electronics.up.railway.app/products/all')
+      .then((res) => { setProduct(res?.data); })
+      .then((res) => console.log(res?.data))
+  }
+  getProducts();
+}, [])
+
+// const rows = [
+//   { id: 1, pDesc: product[1].name, price: 35 },
+//   { id: 2, pDesc: product[2].name, price: 42 },
+//   { id: 3, pDesc: product[3].name, price: 45 },
+//   { id: 4, pDesc: product[4].name, price: 16 },
+//   { id: 5, pDesc: 'Targaryen Daenerys', price: 50 },
+//   { id: 6, pDesc: 'Melisandre', price: 150 },
+//   { id: 7, pDesc: 'Clifford Ferrara', price: 44 },
+//   { id: 8, pDesc: 'Frances Rossini', price: 36 },
+//   { id: 9, pDesc: 'Roxie Harvey', price: 65 },
+// ];
+
+  // const rows=product?.map(item=>{
+  //   return{
+  //     id: item.id,
+  //     imageUrl:item.image,
+  //     pName:item.name,
+  //     pDesc:item.desc,
+  //     price: item.price.toLocaleString()
+  //   };
+  // });
 
 //   const columns = [
 //   { field: 'id', headerName: 'ID', width: 220 },
@@ -45,85 +64,127 @@ const navigate = useNavigate();
 //   width: 80,
 // },
 //   {
-  //     field: 'actions',
-  //     headerName: 'Actions',
-  //     sortable: false,
-  //     width: 170,
-  //     renderCell:(params)=>{
-    //       return(
-      //       <Actions>
-      //         <Delete>Delete</Delete>
-      //         <View>View</View>
-      //       </Actions>
-      //     );
-      //    },
-      //   },
-      // ];
+//       field: 'actions',
+//       headerName: 'Actions',
+//       sortable: false,
+//       width: 170,
+//       renderCell:(params)=>{
+//           return(
+//             <Actions>
+//               <Delete>Delete</Delete>
+//               <View>View</View>
+//             </Actions>
+//           );
+//          },
+//         },
+//       ];
       
-      const rows = [
-        { id: 1, pDesc: 'Snow Jon', price: 35 },
-        { id: 2, pDesc: 'Lannister Cersei', price: 42 },
-        { id: 3, pDesc: 'Lannister Jaime', price: 45 },
-        { id: 4, pDesc: 'Stark Arya', price: 16 },
-        { id: 5, pDesc: 'Targaryen Daenerys', price: 50 },
-        { id: 6, pDesc: 'Melisandre', price: 150 },
-        { id: 7, pDesc: 'Clifford Ferrara', price: 44 },
-        { id: 8, pDesc: 'Frances Rossini', price: 36 },
-        { id: 9, pDesc: 'Roxie Harvey', price: 65 },
-      ];
 
-  const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'imageUrl', headerName: 'Image', width: 80,
-  renderCell:(params)=>{
-  return(
-  <ImageContainer>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjivcct2hLpao-OTDoENHfh_Jbe8GaT2xGzw&usqp=CAU" alt=""/>
-  </ImageContainer>
-  ) 
-  }
-  },
-    {
-    field: 'pDesc',
-    headerName: 'Description',
-    width: 130,
-  },
-  {
-  field: 'price',
-  headerName: 'Price',
-  width: 80,
-},
-  {
-        field: 'actions',
-        headerName: 'Actions',
-        sortable: false,
-        width: 170,
-        renderCell:(params)=>{
-          return(
-          <Actions>
-            <Delete onClick={()=>handledelete(params.row.id)}>Delete</Delete>
-            <EditProduct proId={params.row.id} />
-            <View onClick={()=>navigate(`/product/${params.row.id}`)}>View</View>
-          </Actions>
-        );
-       },
-      },
-];
+//   const columns = [
+//   { field: 'id', headerName: 'ID', width: 70 },
+//   { field: 'imageUrl', headerName: 'Image', width: 80,
+//   renderCell:(params)=>{
+//   return(
+//   <ImageContainer>
+//   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjivcct2hLpao-OTDoENHfh_Jbe8GaT2xGzw&usqp=CAU" alt=""/>
+//   </ImageContainer>
+//   ) 
+//   }
+//   },
+//     {
+//     field: 'pDesc',
+//     headerName: 'Description',
+//     width: 130,
+//   },
+//   {
+//   field: 'price',
+//   headerName: 'Price',
+//   width: 80,
+// },
+//   {
+//         field: 'actions',
+//         headerName: 'Actions',
+//         sortable: false,
+//         width: 170,
+//         renderCell:(params)=>{
+//           return(
+//           <Actions>
+//             <Delete onClick={()=>handledelete(params.row.id)}>Delete</Delete>
+//             <EditProduct proId={params.row.id} />
+//             <View onClick={()=>navigate(`/product/${params.row.id}`)}>View</View>
+//           </Actions>
+//         );
+//        },
+//       },
+// ];
 
-const handledelete=(id)=>{
-  dispatch(productsDelete(id));
-};
+// const handledelete=(id)=>{
+//   dispatch(productsDelete(id));
+// };
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
+    <div>
+      {/* <DataGrid
         rows={rows}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
         disableSelectionOnClick
-      />
+      /> */}
+      {/* <table>
+  <tr>
+    <th>Name</th>
+    <th>Description</th>
+    <th>Price</th>
+  </tr>
+  <tr>
+    <td>16</td>
+    <td>14</td>
+    <td>10</td>
+  </tr>
+</table> */}
+
+<div className="d-flex flex-column">
+        <h1 className="text-white m-3">Products</h1>
+        <table className="table table-dark table-xl m-3">
+          <thead>
+            <tr>
+              <th scope="col">Product ID</th>
+              <th scope="col">Product Name</th>
+              <th scope="col">Image</th>
+              <th scope="col">Price</th>
+              {/* <th scope="col">Edit</th> */}
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+
+  {product?.map(product => {
+          return (
+            <tr>
+              <td>{product.id}</td>
+              <td>{product.name}</td>
+              <ImageContainer>
+   <img src={product?.image} alt=""/>
+   </ImageContainer>
+              <td>₹ {product.price}</td>
+              {/* <td>
+                <button type="button" class="btn btn-success btn-xsm me-2">
+                  Add
+                </button>
+              </td> */}
+              <td>
+                <button type="button" class="btn btn-danger btn-xsm me-2">
+                  Delete
+                </button>
+              </td>
+            </tr>
+    )})}
+            
+          </tbody>
+        </table>
+        </div>
     </div>
   );
 }
--- src/components/admin/list/UsersList.jsx
+++ src/components/admin/list/UsersList.jsx
@@ -1,22 +1,29 @@
 import styled from "styled-components";
 import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import {useDispatch, useSelector} from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { productsDelete } from "../../../slices/productsSlice";
-import EditProduct from "../EditProduct";
-import { useEffect } from "react";
-import { usersFetch } from "../../../slices/usersSlice";
+import { useEffect,useState } from "react";
+import axios from "axios";
+
 
 
 export default function UsersList() {
-  const dispatch = useDispatch();
-const navigate = useNavigate();
+
+  const [product, setProduct] = useState();
+
+useEffect(() => {
+  const getProducts = async () => {
+    await axios.get('https://gada-electronics.up.railway.app/users/all')
+      .then((res) => { setProduct(res?.data); })
+      .then((res) => console.log(res?.data))
+  }
+  getProducts();
+}, [])
+//   const dispatch = useDispatch();
+// const navigate = useNavigate();
   // const {list}=useSelector((state)=>state.users)
 
-    useEffect(()=>{
-        dispatch(usersFetch());
-    },[dispatch])
+    // useEffect(()=>{
+    //     dispatch(usersFetch());
+    // },[dispatch])
 
 //   const rows=list&&list.map((user)=>{
 //     return{
@@ -64,71 +71,105 @@ const navigate = useNavigate();
       //   },
       // ];
       
-      const rows = [
-        { id: 1, pDesc: 'Snow Jon', price: 35 },
-        { id: 2, pDesc: 'Lannister Cersei', price: 42 },
-        { id: 3, pDesc: 'Lannister Jaime', price: 45 },
-        { id: 4, pDesc: 'Stark Arya', price: 16 },
-        { id: 5, pDesc: 'Targaryen Daenerys', price: 50 },
-        { id: 6, pDesc: 'Melisandre', price: 150 },
-        { id: 7, pDesc: 'Clifford Ferrara', price: 44 },
-        { id: 8, pDesc: 'Frances Rossini', price: 36 },
-        { id: 9, pDesc: 'Roxie Harvey', price: 65 },
-      ];
+      // const rows = [
+      //   { id: 1, pDesc: 'Snow Jon', price: 35 },
+      //   { id: 2, pDesc: 'Lannister Cersei', price: 42 },
+      //   { id: 3, pDesc: 'Lannister Jaime', price: 45 },
+      //   { id: 4, pDesc: 'Stark Arya', price: 16 },
+      //   { id: 5, pDesc: 'Targaryen Daenerys', price: 50 },
+      //   { id: 6, pDesc: 'Melisandre', price: 150 },
+      //   { id: 7, pDesc: 'Clifford Ferrara', price: 44 },
+      //   { id: 8, pDesc: 'Frances Rossini', price: 36 },
+      //   { id: 9, pDesc: 'Roxie Harvey', price: 65 },
+      // ];
 
-  const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'imageUrl', headerName: 'Image', width: 80,
-  renderCell:(params)=>{
-  return(
-  <ImageContainer>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjivcct2hLpao-OTDoENHfh_Jbe8GaT2xGzw&usqp=CAU" alt=""/>
-  </ImageContainer>
-  ) 
-  }
-  },
-    {
-    field: 'pDesc',
-    headerName: 'Description',
-    width: 130,
-  },
-  {
-  field: 'price',
-  headerName: 'Price',
-  width: 80,
-},
-  {
-        field: 'actions',
-        headerName: 'Actions',
-        sortable: false,
-        width: 120,
-        renderCell:(params)=>{
-          return(
-          <Actions>
-            <Delete onClick={()=>handledelete(params.row.id)}>Delete</Delete>
-            <View onClick={()=>navigate(`/user/${params.row.id}`)}>View</View>
-          </Actions>
-        );
-       },
-      },
-];
+//   const columns = [
+//   { field: 'id', headerName: 'ID', width: 70 },
+//   { field: 'imageUrl', headerName: 'Image', width: 80,
+//   renderCell:(params)=>{
+//   return(
+//   <ImageContainer>
+//   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjivcct2hLpao-OTDoENHfh_Jbe8GaT2xGzw&usqp=CAU" alt=""/>
+//   </ImageContainer>
+//   ) 
+//   }
+//   },
+//     {
+//     field: 'pDesc',
+//     headerName: 'Description',
+//     width: 130,
+//   },
+//   {
+//   field: 'price',
+//   headerName: 'Price',
+//   width: 80,
+// },
+//   {
+//         field: 'actions',
+//         headerName: 'Actions',
+//         sortable: false,
+//         width: 120,
+//         renderCell:(params)=>{
+//           return(
+//           <Actions>
+//             <Delete onClick={()=>handledelete(params.row.id)}>Delete</Delete>
+//             <View onClick={()=>navigate(`/user/${params.row.id}`)}>View</View>
+//           </Actions>
+//         );
+//        },
+//       },
+// ];
 
-const handledelete=(id)=>{
-//   dispatch(productsDelete(id));
-};
+// const handledelete=(id)=>{
+// //   dispatch(productsDelete(id));
+// };
 
-  return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-        disableSelectionOnClick
-      />
-    </div>
-  );
+return (
+  <div>
+<div className="d-flex flex-column">
+      <h1 className="text-white m-3">Products</h1>
+      <table className="table table-dark table-xl m-3">
+        <thead>
+          <tr>
+            <th scope="col">Product ID</th>
+            <th scope="col">Product Name</th>
+            <th scope="col">Phone No.</th>
+            {/* <th scope="col">Price</th> */}
+            {/* <th scope="col">Edit</th> */}
+            <th scope="col">Role</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+
+{product?.map(product => {
+        return (
+          <tr>
+            <td>{product.id}</td>
+            <td>{product.name}</td>
+            {/* <ImageContainer>
+ <img src={product?.image} alt=""/>
+ </ImageContainer> */}
+            <td>{product.phone}</td>
+            {/* <td>
+              <button type="button" class="btn btn-success btn-xsm me-2">
+                Add
+              </button>
+            </td> */}
+            <td>{(product.role==="ADMIN")?(<Admin>Admin</Admin>):((product.role==="MANAGER")?(<Manager>Manager</Manager>):(<Customer>Customer</Customer>))}</td>
+            <td>
+              <button type="button" class="btn btn-danger btn-xsm me-2">
+                Delete
+              </button>
+            </td>
+          </tr>
+  )})}
+          
+        </tbody>
+      </table>
+      </div>
+  </div>
+);
 }
 
   const ImageContainer = styled.div`
@@ -171,4 +212,11 @@ const handledelete=(id)=>{
   padding:3px 5px;
   border-radius:3px;
   font-size:14px;
+`
+const Manager=styled.div`
+color:rgb(181,253,40);
+background-color:rgb(181,253,40,0.12);
+padding:3px 5px;
+border-radius:3px;
+font-size:14px;
 `
