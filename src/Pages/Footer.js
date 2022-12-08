b6b20deab73205ed46a151ae88a1e726da278fa8
Gaurav <somaigaurav@gmail.com>
2022-12-08 05:31:37 +0530

    footer implemented

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,62 +1,144 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./Footer.css";
 
-
-
-export default class Footer extends Component  {
-   render(){
-      return (
-         
-
-        
-  <footer className="py-5">
-    <div className="row">
-      <div className="col-2">
-        <h5 className="text-dark">Section</h5>
-        <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">About</a></li>
-        </ul>
-      </div>
-
-      <div className="col-2">
-        <h5 className="text-light">Section</h5>
-        <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">About</a></li>
-        </ul>
-      </div>
-
-      <div className="col-2">
-        <h5 className="text-light">Section</h5>
-        <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-light">About</a></li>
-        </ul>
-      </div>
-
-      <div className="col-4 offset-1">
-        <form>
-          <h5 className="text-light">Subscribe to our newsletter</h5>
-          <p className="text-light">Receive notifications of upcoming deals</p>
-          <div className="d-flex w-100 gap-2">
-            <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
-            <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
-            <button className="btn btn-primary" type="button">Subscribe</button>
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer className="py-5 foot">
+        <div className="row">
+          <div className="col-2">
+            <h5 className="text-light">Quick Links</h5>
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <a href="/CustomerHome" className="nav-link p-0 text-light">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="/Profile" className="nav-link p-0 text-light">
+                  Profile
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="/order_history" className="nav-link p-0 text-light">
+                  My Wallet
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="/order_history" className="nav-link p-0 text-light">
+                  Order History
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="/Wishlist" className="nav-link p-0 text-light">
+                  Wishlist
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="/Cart" className="nav-link p-0 text-light">
+                  Cart
+                </a>
+              </li>
+            </ul>
           </div>
-        </form>
-      </div>
-    </div>
 
-    {/* <div className="d-flex justify-content-between py-4 my-4 border-top">
+          <div className="col-2">
+            <h5 className="text-light">About</h5>
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  Ansh Goyal
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  Gaurav Somai
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  Harsh Rachalwar
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  Kumarasamy Chelliah
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  Kush Agrawal
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  Manan Gupta
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-2">
+            <h5 className="text-light">Contact Us</h5>
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  +91 9427458974
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  +91 9427458974
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  +91 9427458974
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  +91 9427458974
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  +91 9427458974
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-light">
+                  +91 9427458974
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-4 offset-1">
+            <form>
+              <h5 className="text-light">Subscribe to our newsletter</h5>
+              <p className="text-light">
+                Receive notifications of upcoming deals
+              </p>
+              <div className="d-flex w-100 gap-2">
+                <label htmlFor="newsletter1" className="visually-hidden">
+                  Email address
+                </label>
+                <input
+                  id="newsletter1"
+                  type="text"
+                  className="form-control"
+                  placeholder="Email address"
+                />
+                <button className="btn btn-primary" type="button">
+                  Subscribe
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* <div className="d-flex justify-content-between py-4 my-4 border-top">
       <ul className="list-unstyled d-flex">
         <li className="ms-3"><a className="link-light" href="#"><svg className="bi" width="24" height="24"><use href="#twitter"></use></svg></a></li>
         <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use href="#instagram"></use></svg></a></li>
@@ -64,7 +146,7 @@ export default class Footer extends Component  {
       </ul>
     </div> */}
 
-    {/* <div className="d-flex justify-content-between py-4 my-4 border-top">
+        {/* <div className="d-flex justify-content-between py-4 my-4 border-top">
       <ul className="list-unstyled d-flex">
         <li className="ms-3"><a className="link-light" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#twitter"></use></svg></a></li>
         <li className="ms-3"><a className="link-light" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#instagram"></use></svg></a></li>
@@ -72,9 +154,7 @@ export default class Footer extends Component  {
       </ul>
       <i className="fab fa-facebook-f"></i>
     </div> */}
-  </footer>
-
-
-      );
-   }
+      </footer>
+    );
+  }
 }
