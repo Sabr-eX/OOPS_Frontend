f171323f00d38b3f1cd015130c20e1ab32b50f04
Harshbphc <92864931+Harshbphc@users.noreply.github.com>
2022-12-08 20:09:28 +0530

    Update Manager.js

--- src/Pages/signin/Manager.js
+++ src/Pages/signin/Manager.js
@@ -1,30 +1,35 @@
-import React, { useRef, useState, useEffect, useContext } from "react";
-import "./Manager.css";
-import axios from "axios";
+import React, { useRef, useState, useEffect, useContext } from "react"
+import './Manager.css'
+import axios from "axios"
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
+
 export default function (props) {
-  let [authMode, setAuthMode] = useState("signin");
   const navigate = useNavigate();
-  function changeAuthMode() {
-    setAuthMode(authMode === "signin" ? "signup" : "signin");
+    let [authMode, setAuthMode] = useState("signin")
+    let [error, setError] = useState(false)
+  let [errorMessage, setErrorMessage] = useState("")
+  
+    const changeAuthMode = () => {
+      setAuthMode(authMode === "signin" ? "signup" : "signin")
+    }
+    let [email,setSignInEmail]= useState("")
+  let [password,setSigninpwd] = useState("")
+  const handlesigninemailchange=(e)=>{
+    setSignInEmail(e.target.value)
   }
-  let [email, setSignInEmail] = useState("");
-  let [password, setSigninpwd] = useState("");
-  const handlesigninemailchange = (e) => {
-    setSignInEmail(e.target.value);
-  };
-  const handlePwdSignInChange = (e) => {
-    setSigninpwd(e.target.value);
-  };
-  function PwdIsValid() {
+  const handlePwdSignInChange=(e)=>{
+    setSigninpwd(e.target.value)
+  }
+  function PwdIsValid(){
     return true;
   }
-  async function signin() {
-    let item = { email, password };
-    console.warn(item);
-    let result3 = await fetch(
+  async function signin (e){
+    e.preventDefault();
+    let item = {email,password}
+    console.warn(item)
+    let result = await fetch(
       "https://gada-electronics.up.railway.app/users/signin",
       {
         method: "POST",
@@ -35,16 +40,23 @@ export default function (props) {
         body: JSON.stringify(item),
       }
     );
-    result3 = await result3.json();
-    localStorage.setItem("user-info", JSON.stringify(result3));
-    result3 = await result3.json();
-    localStorage.setItem("user-info", JSON.stringify(result3));
-    if (!result3.role.localeCompare("MANAGER")) {
-      navigate("/manager");
-    } else {
-      window.alert("sometext");
+    
+    if (result.ok) {
+      result = await result.json();
+      setError(false);
+      localStorage.setItem("user-info", JSON.stringify(result));
+      if (!result.role.localeCompare("MANAGER")) {
+        navigate("/manager");
+      }
     }
-  }
+    else {
+      result = await result.text();
+      setError(true);
+      setErrorMessage(result);
+      // setErrorMessage(result);
+    }
+
+}
   //   axios.post('https://gada-electronics.up.railway.app/users/signin',{
   //        name: emailsignin,
   //        email:signinpwd,
@@ -57,57 +69,67 @@ export default function (props) {
   //    .catch(error => {
   //      alert('service error')
   //    })
+     
+ 
 
-  if (authMode === "signin") {
-    return (
-      <div className="Auth-form-container">
-        <h3 className="position-absolute start-0 top-0 title">Welcome to Shopify</h3>
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Manager Sign In</h3>
-            <div className="form-group mt-3">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control mt-1"
-                placeholder="Enter email"
-                value={email}
-                onChange={handlesigninemailchange}
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter password"
-                value={password}
-                onChange={handlePwdSignInChange}
-              />
-            </div>
-            {/* {
+  
+    if (authMode === "signin") {
+      return (
+        <div className="Auth-form-container">
+          <h3 className="position-absolute start-0 top-0 ms-1">Name</h3>
+          <form className="Auth-form">
+            <div className="Auth-form-content">
+              <h3 className="Auth-form-title" >Manager Sign In</h3>
+              <div className="form-group mt-3">
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className="form-control mt-1"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={handlesigninemailchange}
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control mt-1"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={handlePwdSignInChange}
+                />
+              </div>
+              {/* {
               PwdIsValid() ? <label>Correct</label> :  <label>Incorrect</label>
 
             } */}
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit" onClick={signin} className="btn btn-danger">
-                Submit
+              <div className="d-grid gap-2 mt-3">
+                <button type="submit" onClick = {signin} className="btn btn-danger">
+                  Submit
+                </button>
+                </div>
+                <div className="d-grid gap-2 mt-3">
+              <Link to='/user'><button  className="btn btn-danger">
+                Login as User
               </button>
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <Link to="/user">
-                <button className="btn btn-danger">Login as User</button>
               </Link>
-              <Link to="/adminsignin">
-                <button className="btn btn-danger">Login as Admin</button>
+              <Link to='/adminsignin'><button className="btn btn-danger">
+                Login as Admin
+              </button>
               </Link>
+              </div>
+              <p className="text-center mt-2 text-white">
+              <label>Forgot</label> <a href="#" >password?</a>
+              </p>
+              {error &&
+            <h6>
+              {errorMessage}
+            </h6>
+          }
             </div>
-            <p className="text-center mt-2 text-white">
-              <label>Forgot</label> <a href="#">password?</a>
-            </p>
-          </div>
-        </form>
-      </div>
-    );
+          </form>
+        </div>
+      )
+    }
   }
-}
